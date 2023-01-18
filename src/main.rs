42574e181debdde3491108c54d8d26736ec9ae94
Devin Leamy <devinleamy@gmail.com>
2023-01-18 11:20:43 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Basically, the idea is to put all of my utility scripts for Yabai in here, so that I don't need to fuck around with 'jq' whenever I want to fix something.");
+}
